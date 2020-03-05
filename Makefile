d2cf8a79c0e269c2059be6715cca9f5af97463bb
Stefan Davis <stefan@turnkeylinux.org>
2020-03-05 13:20:38 +1100

    Update codeigniter for v16
    - update codeigniter to 4.0.2
    - replaced tklwebcp in favor for injecting some information into the
        default codeigniter landing page
    - update mysql inithook for new mysql connector (part of turnkeylinux/tracker/issues/1414)

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
-COMMON_OVERLAYS = tkl-webcp
-COMMON_CONF = tkl-webcp
-
 include $(FAB_PATH)/common/mk/turnkey/lamp.mk
 include $(FAB_PATH)/common/mk/turnkey.mk
---
+++ overlay/usr/local/src/codeigniter.overlay/app/Views/_tkl_include.php
@@ -0,0 +1,21 @@
+<p>Routing configuration can be found at:</p>
+
+<pre><code>/var/local/codeigniter/app/Config/Routes.php</code></pre>
+
+<p>Stylesheets, javascript, images and other static assets:</p>
+
+<pre><code>/var/www/codeigniter/public/assets</code></pre>
+
+<p>And MySQL comes preconfigured to get you started
+
+<pre><code>/var/local/codeigniter/app/Config/Database.php</code></pre>
+
+<p>On top of all this, we've also included</p>
+
+<ul>
+    <li><a href="https://<?= $_SERVER{'HTTP_HOST'}; ?>:12320">Web Shell</a></li>
+    <li><a href="https://<?= $_SERVER{'HTTP_HOST'}; ?>:12321">Webmin</a></li>
+    <li><a href="https://<?= $_SERVER{'HTTP_HOST'}; ?>:12322">Adminer</a></li>
+</ul>
+
+<p>Lastly checkout the <a href="https://www.turnkeylinux.org/codeigniter">TurnKey CodeIgniter release notes</a></p>
